0cd881a8bbead793b8f786bf847a7e1d33d55e6c
Ricardo-Emanuel <ricardoemanuelde@gmail.com>
2022-02-09 11:28:44 -0300

    Nova estrutura
    Calculadora funcionando com o laço while e utilizando switch case

---
+++ main.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    int number1, number2;
+    int op = 0;
+    float result;
+
+    cout << "Enter a number: ";
+    cin >> number1;
+
+    while (op != -1){
+
+        cout << "1 - Add operation\n2 - Subtraction operation\n3 - Division operation\n4 - Multiplication operation\n-1 to quit.\n" << endl;
+
+        cout << "Which operation do you want to do? [Choose from the menu] ";
+        cin >> op;
+        if (op == -1){
+            cout << "Thanks for using!";
+            return 0;
+        }
+        cout << "Enter the next number: " << endl;
+        cin >> number2;
+
+
+        switch (op){
+        case 1:{
+            result = number1 + number2;
+            break;
+        }
+        case 2:{
+            result = number1 - number2;
+            break;
+        }
+        case 3:{
+            result = number1 / number2;
+            break;
+        }
+        case 4:{
+            result = number1 * number2;
+            break;
+        }
+        case -1:{
+            return 0;
+            break;
+        }
+        }
+
+
+
+        cout << "\nThe result is: " << result << "\n";
+        cout << "--------------------------------------------------------------\n\n";
+        number1 = result;
+    }
+
+    return 0;
+}
+
