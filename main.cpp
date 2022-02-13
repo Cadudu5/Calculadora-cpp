cca5f7cd18a82eb04e5ad189d0fa7d4d0d0f73b5
Ricardo-Emanuel <ricardoemanuelde@gmail.com>
2022-02-13 13:59:33 -0300

    Adicionando uma limpeza de prompt ao usuario

--- main.cpp
+++ main.cpp
@@ -22,54 +22,65 @@ int calculator(int op)
             cout << "Enter the next number: " << endl;
             cin >> number2;
             result = number1 + number2;
+            system("cls");
             break;
         }
         case 2:{
             cout << "Enter the next number: " << endl;
             cin >> number2;
             result = number1 - number2;
+            system("cls");
             break;
         }
         case 3:{
             cout << "Enter the next number: " << endl;
             cin >> number2;
             result = number1 / number2;
+            system("cls");
             break;
         }
         case 4:{
             cout << "Enter the next number: " << endl;
             cin >> number2;
             result = number1 * number2;
+            system("cls");
             break;
         }
         case 5:{
             result = number1 * number1;
+            system("cls");
             break; 
         }
         case 6: {
             result = sqrt(number1);
+            system("cls");
             break;
         }
         case 7: {
             cout << "Enter the exponent: ";
             cin >> number2;
             result = pow(number1, number2);
+            system("cls");
             break;
         }
         case 8: {
             result = tgamma(number1 + 1);
+            system("cls");
             break;
         }
         case 9: {
             result = cos(number1);
+            system("cls");
             break;
         }
         case 10: {
             result = sin(number1);
+            system("cls");
             break;
         }
         case 11: {
             result = tan(number1);
+            system("cls");
             break;
         }
         case -1: {
@@ -93,7 +104,9 @@ int main()
     int op;
     while (op != -2){
         op = calculator(op);
+        system("cls");
     }
+    system("cls");
     cout << "Thanks for using!";
     return 0;
 }
