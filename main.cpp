d588712187022ae542ee13351d3fae34465a1930
Ricardo-Emanuel <ricardoemanuelde@gmail.com>
2022-02-16 10:25:41 -0300

    Mudanças nas variáveis e nas funcoes trig
    Agora cada funcao trigonometrica tem a conversao de graus pra radianos,
    utilizacao do printf no output do resultado, mudanca no menu e nos
    comandos.

--- main.cpp
+++ main.cpp
@@ -2,10 +2,12 @@
 #include <cmath>
 using namespace std;
 
+const int PI = 3.1415626535;
 
-int calculator(int op)
+
+int calculator(char op)
 {
-    int number1, number2;
+    float number1, number2;
 
     float result;
 
@@ -13,108 +15,114 @@ int calculator(int op)
     cout << "Enter the first number: ";
     cin >> number1;
 
-    while (op != -2){
+    while (op != 'q'){
         cout << "\n\t\t\tMENU ";  
         cout << "\n_______________________________________________________" << endl;
-        cout << "(1) Add operation\t| (2) Subtraction operation\n(3) Division operation\t| (4) Multiplication operation\n";
-        cout << "(5) Square\t\t| (6) Square root\n(7) Exponenciation\t| (8) Factorial\n(9) Cosine(deg)\t\t| (10) Sine(deg)\n(11) tangent(deg)\t|\n" << endl; 
-        cout << "Type (-1) to erase or Type (-2) to quit...";
+        cout << "(+) Add operation\t| (-) Subtraction operation\n(/) Division operation\t| (*) Multiplication operation\n";
+        cout << "(1) Square\t\t| (s) Square root\n(^) Exponenciation\t| (!) Factorial\n(2) Cosine(deg)\t\t| (3) Sine(deg)\n(4) tangent(deg)\t|\n" << endl; 
+        cout << "Type (e) to erase or Type (q) to quit...";
         cout << "\n--------------------------------------------------------" << endl;
         cout << "Which operation do you want to do? [Choose from the menu] ";
         cin >> op;
         switch (op){
-        case 1:{
+        case '+':{
             cout << "Enter the next number: " << endl;
             cin >> number2;
             result = number1 + number2;
             system("cls");
             cout << "\n" << number1 << " + " << number2 << " = " << result << "\n";
-            cout << "--------------------------------------------------------------\n\n";
+            cout << "--------------------------------------------------------------\n";
             break;
         }
-        case 2:{
+        case '-':{
             cout << "Enter the next number: " << endl;
             cin >> number2;
             result = number1 - number2;
             system("cls");
             cout << "\n" <<  number1 << " - " << number2 << " = " << result << "\n";
-            cout << "--------------------------------------------------------------\n\n";
+            cout << "--------------------------------------------------------------\n";
             break;
         }
-        case 3:{
+        case '/':{
             cout << "Enter the next number: " << endl;
             cin >> number2;
             result = number1 / number2;
             system("cls");
             cout << "\n" << number1 << "/" << number2 << " = " << result << "\n";
-            cout << "--------------------------------------------------------------\n\n";
+            cout << "--------------------------------------------------------------\n";
             break;
         }
-        case 4:{
+        case '*':{
             cout << "Enter the next number: " << endl;
             cin >> number2;
             result = number1 * number2;
             system("cls");
             cout << "\n" << number1 << "*" << number2 << " = " << result << "\n";
-            cout << "--------------------------------------------------------------\n\n";
+            cout << "--------------------------------------------------------------\n";
             break;
         }
-        case 5:{
+        case '1':{
             result = number1 * number1;
             system("cls");
             cout << "\n" << "(" << number1 << ")" << "^" << 2 << " = " << result << "\n";
-            cout << "--------------------------------------------------------------\n\n";
+            cout << "--------------------------------------------------------------\n";
             break; 
         }
-        case 6: {
+        case 's': {
             result = sqrt(number1);
             system("cls");
             cout << "\n" << "square root(" << number1 << ")" << " = " << result << "\n";
-            cout << "--------------------------------------------------------------\n\n";
+            cout << "--------------------------------------------------------------\n";
             break;
         }
-        case 7: {
+        case '^': {
             cout << "Enter the exponent: ";
             cin >> number2;
             result = pow(number1, number2);
             system("cls");
             cout << "\n" << "(" << number1 << ")" << "^" << number2 << " = " << result << "\n";
-            cout << "--------------------------------------------------------------\n\n";
+            cout << "--------------------------------------------------------------\n";
             break;
         }
-        case 8: {
+        case '!': {
             result = tgamma(number1 + 1);
             system("cls");
             cout << "\n" << number1 << "!" <<  "= " << result << "\n";
-            cout << "--------------------------------------------------------------\n\n";
+            cout << "--------------------------------------------------------------\n";
             break;
         }
-        case 9: {
-            result = cos(number1);
+        case '2': {
+            float radians;
+            radians = number1 * 0.017453292519;
+            result = cos(radians);
             system("cls");
-            cout << "\n" << "cos(" << number1 << ")" << " = " << result << "\n";
-            cout << "--------------------------------------------------------------\n\n";
+            printf("cos(%.2f) = %.3f\n", number1, result);
+            cout << "--------------------------------------------------------------\n";
             break;
         }
-        case 10: {
-            result = sin(number1);
+        case '3': {
+            float radians;
+            radians = number1 * 0.017453292519;
+            result = sin(radians);
             system("cls");
-            cout << "\n" << "sin(" << number1 << ")" << " = " << result << "\n";
-            cout << "--------------------------------------------------------------\n\n";
+            printf("sin(%.2f) = %.3f\n", number1, result);
+            cout << "--------------------------------------------------------------\n";
             break;
         }
-        case 11: {
-            result = tan(number1);
+        case '4': {
+            float radians;
+            radians = number1 * 0.017453292519;
+            result = tan(radians);
             system("cls");
-            cout << "\n" << "tan(" << number1 << ")" << " = " << result << "\n";
-            cout << "--------------------------------------------------------------\n\n";
+            printf("tan(%.2f) = %.3f\n", number1, result);
+            cout << "--------------------------------------------------------------\n";
             break;
         }
-        case -1: {
+        case 'e': {
             return op;
             break;
         }
-        case -2: {
+        case 'q': {
             return op;
             break;
         }
@@ -127,9 +135,9 @@ int calculator(int op)
 
 int main()
 {
-    int op;
+    char op;
     cout << "\nWelcome to the Calculator!\n";
-    while (op != -2){
+    while (op != 'q'){
         op = calculator(op);
         system("cls");
     }
